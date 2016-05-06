27c97b597eeb5ea7be7fe1bda20bc42beb86c6b4
ksrubka <beata.choluj@wp.pl>
2016-05-06 20:39:01 +0200

    Refactor variables names

--- src/exponentiation/Exponentiation.java
+++ src/exponentiation/Exponentiation.java
@@ -3,6 +3,7 @@ package exponentiation;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Beata Iłowiecka on 25.04.16.
@@ -10,16 +11,17 @@ import java.util.List;
 public class Exponentiation {
 
     public static void main(String[] args) {
-        byte numOfCases = getNrOfCases();
-        List<BigInteger> results = determineCases(numOfCases);
+        Scanner s = new Scanner(System.in);
+        byte numOfLines = getNrOfLines();
+        List<BigInteger> results = determineCases(numOfLines);
         getExpResults(results);
     }
 
-    private static byte getNrOfCases() {
-        byte numOfCases = (byte) (Math.random() * 10 + 1);
+    private static byte getNrOfLines() {
+        byte numOfLines = (byte) (Math.random() * 10 + 1);
         //liczba przypadków
-        System.out.println(numOfCases);
-        return numOfCases;
+        System.out.println(numOfLines);
+        return numOfLines;
     }
 
     private static List<BigInteger> determineCases(byte numOfCases) {
