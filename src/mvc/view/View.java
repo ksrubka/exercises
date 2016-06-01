be3eb91fbaaf902dddc17a73dd49adcb3f7ff6d1
ksrubka <beata.choluj@wp.pl>
2016-06-01 15:23:36 +0200

    Add second button
    With anonymous class implementing ActionListener

--- src/mvc/view/View.java
+++ src/mvc/view/View.java
@@ -14,23 +14,41 @@ public class View extends JFrame implements ActionListener {
 
     private Model model;
     private JButton helloButton;
+    private JButton byeButton;
 
     public View(Model model) throws HeadlessException {
         super("MVC Demo");
         this.model = model;
         helloButton = new JButton("Click Me!");
+        byeButton = new JButton("Bye!");
 
         setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.anchor = GridBagConstraints.CENTER;
-        gbc.gridheight = 0;
-        gbc.gridwidth = 0;
+        gbc.gridx = 2;
+        gbc.gridy = 1;
         gbc.weightx = 1;
         gbc.weighty=1;
         gbc.fill= GridBagConstraints.NONE;
 
         add(helloButton, gbc);
         helloButton.addActionListener(this);
+
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.weightx = 1;
+        gbc.weighty=1;
+        gbc.fill= GridBagConstraints.NONE;
+
+        add(byeButton, gbc);
+        byeButton.addActionListener(this);
+        byeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Don't go :)");
+            }
+        });
 
         setSize(600, 600);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
