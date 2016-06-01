8ea5af662b03a06aeafd457731ff8ad88c2ffbf2
ksrubka <beata.choluj@wp.pl>
2016-06-01 17:08:17 +0200

    Delete Model and controller cause YAGNI

--- src/observer/Application.java
+++ src/observer/Application.java
@@ -20,9 +20,7 @@ public class Application {
     }
 
     public static void runApp() {
-        Model model = new Model();
-        View view = new View(model);
-        Controller controller = new Controller(model, view);
+        View view = new View();
     }
 }
 
